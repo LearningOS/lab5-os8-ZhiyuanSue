9edd5e7957f4534545b390b0552a5f3115dedb12
Yi Zhu <zhiyuansue@192.168.0.195>
2022-07-30 07:09:41 +0800

    finish code,need debug

---
+++ os8/src/sync/DL_Detect.rs
@@ -0,0 +1,107 @@
+pub const MATRIX_AXIS :usize=128;
+pub const SEMA_BASE:usize=64;
+pub struct DL_detect{
+    pub row_size:usize,
+    pub have_enable_deadlock_detect:bool,
+    //vecs
+    pub all:[isize;MATRIX_AXIS],
+    pub avaliable:[isize;MATRIX_AXIS],
+    //matrixes
+    //here row is the thread
+    //column is the semaphore
+    pub allocation:[[isize;MATRIX_AXIS];MATRIX_AXIS],
+    pub need:[[isize;MATRIX_AXIS];MATRIX_AXIS],
+}
+
+impl DL_detect{
+    pub fn new()->Self{
+        Self{
+            row_size:0,
+            have_enable_deadlock_detect:false,
+            //vecs
+            all:[0;MATRIX_AXIS],
+            avaliable:[0;MATRIX_AXIS],
+            //matrixes
+            allocation:[[0;MATRIX_AXIS];MATRIX_AXIS],
+            need:[[0;MATRIX_AXIS];MATRIX_AXIS],
+        }
+    }
+    pub fn is_DL_activitive(&self)->bool
+    {
+        self.have_enable_deadlock_detect
+    }
+    pub fn add_resource(&mut self,column_id:usize,resource_num:isize)
+    {
+        self.all[column_id]=resource_num;
+        self.avaliable[column_id]=resource_num;
+    }
+    pub fn detect(&self,column_id:usize) -> bool
+    {
+        let mut work=[0;MATRIX_AXIS];
+        let mut finish=[false;MATRIX_AXIS];
+        for i in 0..MATRIX_AXIS{
+            work[i]=self.avaliable[i];
+        }
+        loop{
+            let mut find_thread=false;
+            for i in 0..self.row_size
+            {
+                if finish[i]==false
+                {
+                    let mut all_need_satisify=true;
+                    for j in 0..MATRIX_AXIS{
+                        if self.need[i][j]>work[j]
+                        {
+                            all_need_satisify=false;
+                            break;
+                        }
+                    }
+                    if all_need_satisify{
+                        for j in 0..MATRIX_AXIS
+                        {
+                            work[j]=work[j]+self.allocation[i][j];
+                        }
+                        finish[i]=true;
+                        find_thread=true;
+                        break;
+                    }
+                }
+            }
+            if find_thread==false{
+                return false;
+            }
+            let mut all_finish=true;
+            for i in 0..self.row_size
+            {
+                if finish[i]==false{
+                    all_finish=false;
+                    break;
+                }
+            }
+            if all_finish{
+                return true;
+            }
+        }
+    }
+    pub fn alloc_lock(&mut self,column_id:usize,row_id:usize)->isize
+    {
+        self.avaliable[column_id]+=1;
+        self.allocation[row_id][column_id]-=1;
+        self.need[row_id][column_id]-=1;
+        if self.allocation[row_id][column_id]<0
+        {
+            return -1;
+        }
+        0
+    }
+    pub fn free_lock(&mut self,column_id:usize,row_id:usize)->isize
+    {
+        self.avaliable[column_id]-=1;
+        self.allocation[row_id][column_id]+=1;
+        0
+    }
+    pub fn add_need(&mut self,column_id:usize,row_id:usize)
+    {
+        self.need[row_id][column_id]+=1;
+    }
+}
---
+++ os8/src/syscall/sync.rs
@@ -0,0 +1,218 @@
+use crate::sync::{Condvar, Mutex, MutexBlocking, MutexSpin, Semaphore,SEMA_BASE};
+use crate::task::{block_current_and_run_next, current_process, current_task};
+use crate::timer::{add_timer, get_time_ms};
+use alloc::sync::Arc;
+
+
+pub fn sys_sleep(ms: usize) -> isize {
+    let expire_ms = get_time_ms() + ms;
+    let task = current_task().unwrap();
+    add_timer(expire_ms, task);
+    block_current_and_run_next();
+    0
+}
+
+// LAB5 HINT: you might need to maintain data structures used for deadlock detection
+// during sys_mutex_* and sys_semaphore_* syscalls
+pub fn sys_mutex_create(blocking: bool) -> isize {
+    let process = current_process();
+    let mutex: Option<Arc<dyn Mutex>> = if !blocking {
+        Some(Arc::new(MutexSpin::new()))
+    } else {
+        Some(Arc::new(MutexBlocking::new()))
+    };
+    let mut process_inner = process.inner_exclusive_access();
+    if let Some(id) = process_inner
+        .mutex_list
+        .iter()
+        .enumerate()
+        .find(|(_, item)| item.is_none())
+        .map(|(id, _)| id)
+    {
+        if process_inner.DL_detect.is_DL_activitive()
+        {
+            process_inner.DL_detect.add_resource(id, 1);
+        }
+        process_inner.mutex_list[id] = mutex;
+        id as isize
+    } else {
+        process_inner.mutex_list.push(mutex);
+        let id =process_inner.mutex_list.len() as isize - 1;
+        if process_inner.DL_detect.is_DL_activitive()
+        {
+            process_inner.DL_detect.add_resource(id as usize, 1);
+        }
+        id
+    }
+}
+
+// LAB5 HINT: Return -0xDEAD if deadlock is detected
+pub fn sys_mutex_lock(mutex_id: usize) -> isize {
+    let process = current_process();
+    let mut process_inner = process.inner_exclusive_access();
+    let mutex = Arc::clone(process_inner.mutex_list[mutex_id].as_ref().unwrap());
+    if process_inner.DL_detect.is_DL_activitive()
+    {
+        let task = current_task().unwrap();
+        let task_id=task.inner_exclusive_access().res.as_ref().unwrap().tid;
+        process_inner.DL_detect.add_need(mutex_id, task_id);
+        if process_inner.DL_detect.detect(mutex_id) ==false
+        {
+            return -0xDEAD;
+        }
+        else
+        {
+            process_inner.DL_detect.alloc_lock(mutex_id,task_id);
+        }
+    }
+    drop(process_inner);
+    drop(process);
+    mutex.lock();
+    0
+}
+
+pub fn sys_mutex_unlock(mutex_id: usize) -> isize {
+    let process = current_process();
+    let mut process_inner = process.inner_exclusive_access();
+    let mutex = Arc::clone(process_inner.mutex_list[mutex_id].as_ref().unwrap());
+    if process_inner.DL_detect.is_DL_activitive()
+    {
+        let task = current_task().unwrap();
+        let task_id=task.inner_exclusive_access().res.as_ref().unwrap().tid;
+        process_inner.DL_detect.free_lock(mutex_id,task_id);
+    }
+    drop(process_inner);
+    drop(process);
+    mutex.unlock();
+    0
+}
+
+pub fn sys_semaphore_create(res_count: usize) -> isize {
+    let process = current_process();
+    let mut process_inner = process.inner_exclusive_access();
+
+    let id = if let Some(id) = process_inner
+        .semaphore_list
+        .iter()
+        .enumerate()
+        .find(|(_, item)| item.is_none())
+        .map(|(id, _)| id)
+    {
+        process_inner.semaphore_list[id] = Some(Arc::new(Semaphore::new(res_count)));
+        if process_inner.DL_detect.is_DL_activitive()
+        {
+            process_inner.DL_detect.add_resource(id+SEMA_BASE, res_count as isize);
+        }
+        id
+    } else {
+        process_inner
+            .semaphore_list
+            .push(Some(Arc::new(Semaphore::new(res_count))));
+        let id=process_inner.semaphore_list.len() - 1;
+        if process_inner.DL_detect.is_DL_activitive()
+        {
+            process_inner.DL_detect.add_resource(id+SEMA_BASE, res_count as isize);
+        }
+        id
+    };
+    id as isize
+}
+
+pub fn sys_semaphore_up(sem_id: usize) -> isize {
+    let process = current_process();
+    let mut process_inner = process.inner_exclusive_access();
+    let sem = Arc::clone(process_inner.semaphore_list[sem_id].as_ref().unwrap());
+    if process_inner.DL_detect.is_DL_activitive()
+    {
+        let task = current_task().unwrap();
+        let task_id=task.inner_exclusive_access().res.as_ref().unwrap().tid;
+        process_inner.DL_detect.add_need(sem_id+SEMA_BASE, task_id);
+        if process_inner.DL_detect.detect(sem_id+SEMA_BASE) ==false
+        {
+            return -0xDEAD;
+        }
+        else
+        {
+            process_inner.DL_detect.alloc_lock(sem_id+SEMA_BASE,task_id);
+        }
+        
+    }
+    drop(process_inner);
+    sem.up();
+    0
+}
+
+// LAB5 HINT: Return -0xDEAD if deadlock is detected
+pub fn sys_semaphore_down(sem_id: usize) -> isize {
+    let process = current_process();
+    let mut process_inner = process.inner_exclusive_access();
+    let sem = Arc::clone(process_inner.semaphore_list[sem_id].as_ref().unwrap());
+    if process_inner.DL_detect.is_DL_activitive()
+    {
+        let task = current_task().unwrap();
+        let task_id=task.inner_exclusive_access().res.as_ref().unwrap().tid;
+        process_inner.DL_detect.free_lock(sem_id+SEMA_BASE,task_id);
+    }
+    drop(process_inner);
+    sem.down();
+    0
+}
+
+pub fn sys_condvar_create(_arg: usize) -> isize {
+    let process = current_process();
+    let mut process_inner = process.inner_exclusive_access();
+    let id = if let Some(id) = process_inner
+        .condvar_list
+        .iter()
+        .enumerate()
+        .find(|(_, item)| item.is_none())
+        .map(|(id, _)| id)
+    {
+        process_inner.condvar_list[id] = Some(Arc::new(Condvar::new()));
+        id
+    } else {
+        process_inner
+            .condvar_list
+            .push(Some(Arc::new(Condvar::new())));
+        process_inner.condvar_list.len() - 1
+    };
+    id as isize
+}
+
+pub fn sys_condvar_signal(condvar_id: usize) -> isize {
+    let process = current_process();
+    let process_inner = process.inner_exclusive_access();
+    let condvar = Arc::clone(process_inner.condvar_list[condvar_id].as_ref().unwrap());
+    drop(process_inner);
+    condvar.signal();
+    0
+}
+
+pub fn sys_condvar_wait(condvar_id: usize, mutex_id: usize) -> isize {
+    let process = current_process();
+    let process_inner = process.inner_exclusive_access();
+    let condvar = Arc::clone(process_inner.condvar_list[condvar_id].as_ref().unwrap());
+    let mutex = Arc::clone(process_inner.mutex_list[mutex_id].as_ref().unwrap());
+    drop(process_inner);
+    condvar.wait(mutex);
+    0
+}
+
+// LAB5 YOUR JOB: Implement deadlock detection, but might not all in this syscall
+pub fn sys_enable_deadlock_detect(_enabled: usize) -> isize {
+    if _enabled!=1&&_enabled!=0{
+        return -1;
+    }
+    let process = current_process();
+    let mut process_inner = process.inner_exclusive_access();
+    if _enabled==1{
+        process_inner.DL_detect.have_enable_deadlock_detect=true;
+    }
+    else if _enabled==0{
+        process_inner.DL_detect.have_enable_deadlock_detect=false;
+    }
+    else{
+        return -1;
+    }
+    0
+}
